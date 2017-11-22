a96cda79a6bcb97efdcf4b76726b435009818a82
Márton Török <tormartonz@gmail.com>
2017-11-22 09:35:52 +0100

    ok

--- gittie.py
+++ gittie.py
@@ -3,9 +3,7 @@ def add(x, y):
 
 
 def introduce():
- HEAD
     print("Hello, I'm Gittie! I used to Git up early, but now I barely Git out of bed,I am also vegan")
- 49068238065ef26510b3b58f09f893868dbfc02d
 
 
 def joke():
